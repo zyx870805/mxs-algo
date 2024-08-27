fccdc969c4dccf99f6169b4f25440d1d460bad48
zyx <zyx870805@163.com>
2024-08-27 08:51:02 +0800

    double link list

--- src/main/java/com/xm/list/DoubleLinkedList.java
+++ src/main/java/com/xm/list/DoubleLinkedList.java
@@ -38,4 +38,59 @@ public class DoubleLinkedList {
         }
         
     }
+
+    public void append(int newData) {
+        Node newNode = new Node(newData);
+        Node last = head;
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        while(last.next != null) {
+            last = last.next;
+        }
+        last.next = newNode;
+        newNode.pre = last;
+    }
+
+    public void insertBefore(Node nextNode, int newData) {
+        if (nextNode == null) {
+            System.out.println("nextNode 不能为null");
+            return;
+        }
+        Node newNode = new Node(newData);
+        newNode.pre = nextNode.pre;
+        nextNode.pre = newNode;
+        newNode.next = nextNode;
+        if (newNode.pre != null) {
+            newNode.pre.next = newNode;
+        } else {
+            head = newNode;
+        }
+    }
+
+    public void deleteNode(Node head, Node del) {
+        if (head == null || del == null) {
+            return;
+        }
+        if (head == del) {
+            head = del.next;
+        }
+        if (del.next != null) {
+            del.next.pre = del.pre;
+        }
+        if (del.pre != null) {
+            del.pre.next = del.next;
+        }
+    }
+
+    public void printList(Node head) {
+        Node node = head;
+        while(node != null) {
+            System.out.print(node.data);
+            node  = node.next;
+        }
+    }
+
+    
 }
